ba118e58aeda8d7b474aa4d223b1cce899926a15
hamdin <hamdingenc1989@gmail.com>
2023-02-20 13:43:35 -0800

    1st commit

---
+++ src/GitProject01/Company.java
@@ -0,0 +1,11 @@
+package GitProject01;
+
+public class Company {
+    public static void main(String[] args) {
+            //write your code here.
+
+            System.out.println("I am Groot!!!");
+        }
+
+
+    }
